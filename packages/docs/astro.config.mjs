1063ae12e4339a88918077fdcd3fb73f3672a840
righ <django.hamster@gmail.com>
2026-04-25 01:54:05 +0900

    fix: domain

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jupyter-kit.walkframe.com',
   integrations: [
     starlight({
       title: '@jupyter-kit',
